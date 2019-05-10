c8521c0481a80eb1bb2fc5d2d49c292465658aa6
Marcelo Martins <727100@gmail.com>
2019-05-09 22:26:48 -0300

    0104 operadores igualdade e relacionais

---
+++ aula01/04-op_igualdade-relacionais.js
@@ -0,0 +1,21 @@
+operadores de igualdade e relacionais
+
+== igualdade de valores
+!= diferente de
+
+
+=== igualdade de valores e de tipo
+!== diferente, mas do mesmo tipo
+
+
+1 == '1' true
+1 === '1' false
+1 !== '1' true
+
+
+>
+<
+>=
+<=
+
+
