8cec64865b6759b6ef0e6257a56a331154c00dd9
Marcelo Martins <727100@gmail.com>
2019-05-09 22:00:45 -0300

    0102 variaveis e tipos de dados

---
+++ aula01/tipos_dados.js
@@ -0,0 +1,26 @@
+Number
+String
+Boolean
+null
+undefined
+
+/*
+ * ctrl + L limpa console Node 
+*/
+
+{} Object
+
+//exemplo
+var pessoa = {}
+pessoa = {
+    altura : 1.87,
+    peso: 90
+}
+pessoa.altura //acesso altura, dentro do objeto pessoa
+pessoa.altura = 1.90
+pessoa['altura'] = 1.90 //acessando altura como array
+
+[] Array
+var numeros = []
+numeros = [1,2,3,4,5,6]
+numeros[0] //acessa o indice "0" do array no exemplo, "1"
