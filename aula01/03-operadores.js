8ecaee8e8881c552cbdffd88edf457b902a418bb
Marcelo Martins <727100@gmail.com>
2019-05-09 22:16:40 -0300

    0103 operadores aritimeticos

---
+++ aula01/03-operadores.js
@@ -0,0 +1,20 @@
+Operadores Artimeticos
+
++ Adição
+- Subtração
+* Multiplicação
+/ Divisão
+
+Operadores Artimeticos abreviados
+++ 
+--
++=
+-=
+*=
+/=
+
+
+valor++ ou ++valor
+valor-- ou --valor
+valor+=
+valor -= 20
