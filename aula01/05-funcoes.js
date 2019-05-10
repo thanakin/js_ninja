6fc679cbfa3cb55fce49b10ef844ca879108441d
Marcelo Martins <727100@gmail.com>
2019-05-09 22:37:52 -0300

    0105 funcoes

---
+++ aula01/05-funcoes.js
@@ -0,0 +1,35 @@
+//Declarando função
+function nome () {
+    //conteudo
+}
+
+//chamando função
+nome()
+
+//exemplo
+var x = 1
+undefined
+function  soma() {
+    x = x + 1;
+    }
+undefined
+x
+1
+soma()
+undefined
+x
+2
+
+//passando parametros/argumentos para função
+
+function soma (x, y) {
+    return x + y;
+    }
+undefined
+soma()
+NaN //Noth a Number
+soma(1, 2)
+3
+
+
+
